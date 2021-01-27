95e38dd142967f472fd54075139a0d63936d2e20
Kiloku <mateus.sateles@gmail.com>
2021-01-27 14:15:56 -0200

    Button Prompt for when near vendor

---
+++ Assets/Scripts/ButtonPrompt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPrompt : MonoBehaviour
+{
+    public GameObject Target;
+    public Vector2 offset;
+    private Camera mainCamera;
+    private void Start()
+    {
+        mainCamera = Camera.main;;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null && mainCamera != null)
+        {
+            transform.position = (Vector2) mainCamera.WorldToScreenPoint(Target.transform.position ) + offset;
+        }
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -8,6 +8,10 @@ public class PlayerControl : MonoBehaviour
     private CustomizableCharacter Character;
 
     public CompositeAnimator animator;
+
+    public GameObject ShopUI;
+
+    public GameObject SpacePrompt;
     
     private void Start()
     {
@@ -15,11 +19,20 @@ public class PlayerControl : MonoBehaviour
         animator.CurrentAnimationName = "Down";
     }
 
+    private bool NearVendor;
+
     // Update is called once per frame    
     void Update()
     {
         AnimatorUpdate();
         transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0)* Time.deltaTime, Space.Self) ;
+
+        
+        if (NearVendor && Input.GetKeyDown(KeyCode.Space))
+        {
+            ShopUI.SetActive(true);
+            SpacePrompt.SetActive(false);
+        }
     }
 
     void AnimatorUpdate()
@@ -50,5 +63,24 @@ public class PlayerControl : MonoBehaviour
             if (!animator.playing) animator.Play();
         }
 
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "vendor")
+        {
+            SpacePrompt.SetActive(true);
+            NearVendor = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "vendor")
+        {
+            SpacePrompt.SetActive(false);
+            NearVendor = false;
+        }
     }
 }
